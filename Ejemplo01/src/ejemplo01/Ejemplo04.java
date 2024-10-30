812b219b7c765966e5d7634a64b3e468ab8d9493
Kenny Paredes <jkparedes3@utpl.edu.ec>
2024-10-30 11:42:28 -0500

    comentario2

--- Ejemplo01/src/ejemplo01/Ejemplo04.java
+++ Ejemplo01/src/ejemplo01/Ejemplo04.java
@@ -18,5 +18,7 @@ public class Ejemplo04 {
         System.out.print(apellidoEstudiante + " ");
         System.out.print(nacimiento + " ");
         System.out.print(nombreEstudiante + " ");
+        // println hace que me de un salto de una linea a otra 
+        // print hace que no de ningun salto a otra linea me mantengo en la misma linea
     }
 }
