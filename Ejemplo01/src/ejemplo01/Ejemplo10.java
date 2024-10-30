2ba8ea9d1d3565447231c0ac87594f461cc1df28
Kenny Paredes <jkparedes3@utpl.edu.ec>
2024-10-30 11:56:02 -0500

    ejercicio10

--- Ejemplo01/src/ejemplo01/Ejemplo10.java
+++ Ejemplo01/src/ejemplo01/Ejemplo10.java
@@ -11,13 +11,13 @@ package ejemplo01;
  *
  * @author reroes
  */
-public class Ejemplo05 {
+public class Ejemplo10 {
     public static void main(String[] args) {
         // Secuencia de escape \n y \t
-        String nombreEstudiante = "René Rolando";
-        String apellidoEstudiante = "Elizalde Solano";
-        int nacimiento = 1983;
-        
+        String nombreEstudiante = "Jefferson Kenny";
+        String apellidoEstudiante = "Paredes Jara";
+        int nacimiento = 2005;
+        int edad = 18;
         /*System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
                 nacimiento);
         
@@ -27,6 +27,6 @@ public class Ejemplo05 {
         System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
                 "\n"+ nacimiento); */
         System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
-        System.out.println(nombreEstudiante+"\n\n\t"+apellidoEstudiante);   
+        System.out.println(nombreEstudiante+"\n\n\t"+edad);   
     }
 }
